0c8ad78582ebd23822e64ba7557efaa46258a6aa
Gokulakrishnan Bharathiraja <140400289+gokulakrish12@users.noreply.github.com>
2025-09-17 18:08:32 +0530

    commit 1

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  
+  
+  const hamburger = document.querySelector('.hamburger');
+const navlinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navlinks.classList.toggle('active');
+});
+
